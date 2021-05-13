5327614a54b61f3fa61f259955f3b9278ddb7f88
Antonello <antonello.galipo@crispybacon.it>
2021-05-13 16:34:55 +0200

    fixed navigation persistence across hot reloads

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,6 +4,7 @@ import 'package:navigator2/app/dependency_injection/dependency_provider.dart';
 import 'package:navigator2/app/routing/main_navigation.dart';
 import 'package:navigator2/app/routing/main_navigation_stack.dart';
 import 'package:navigator2/app/routing/main_router_delegate.dart';
+import 'package:navigator2/app/routing/routes.dart';
 
 import 'routing/main_router_information_parser.dart';
 
@@ -11,6 +12,9 @@ class MyApp extends StatelessWidget {
   final delegate =
       MainRouterDelegate(MainNavigationStack([MainNavigation.splash()]));
   final parser = MainRouterInformationParser();
+  final p = PlatformRouteInformationProvider(
+    initialRouteInformation: RouteInformation(location: '/${Routes.HOME}'),
+  );
   @override
   Widget build(BuildContext context) {
     return DependencyProvider(
@@ -19,6 +23,7 @@ class MyApp extends StatelessWidget {
         builder: (context) => MaterialApp.router(
           routerDelegate: delegate,
           routeInformationParser: parser,
+          routeInformationProvider: p,
         ),
       ),
     );
